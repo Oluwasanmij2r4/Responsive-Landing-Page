20fae7b0c6e732230e7f270128b565c4b315b52a
Oluwasanmij2r4 <137338488+Oluwasanmij2r4@users.noreply.github.com>
2024-10-31 16:00:41 +0100

    I did the full responsiveness of thw whole page with a little js for the scrolling and the menu-bar

--- main.js
+++ main.js
@@ -13,3 +13,22 @@ document.getElementById("close-menu").addEventListener("click", function (e) {
   e.preventDefault();
   document.querySelector(".nav_links").classList.remove("active");
 });
+
+// smooth scrolling
+$('.nav_links a').on('click', function(event){
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top
+            },
+            800,
+            function(){
+                window.location.hash = hash;
+            }
+        );
+    }
+});
